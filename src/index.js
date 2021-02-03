4c028e1eacfd9d5dde73f82a568ac33a0199bfc9
edougher <ejdougher@gmail.com>
2021-02-02 23:04:09 -0600

    temp

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+const newTaskForm = document.getElementById("create-task-form");
+newTaskForm.addEventListener("sumbit", function (e) {
+  e.preventDefault()
+  console.log("helloworld")      
+  //let item = document.createElement("li"); // create li node
+  //let input = document.querySelector("#new-task-description");
+  //let itemText = document.createTextNode(input.value); // create text node
+  //let tasks = document.getElementById("tasks");
+  //
+  //
+  //item.appendChild(itemText); // append text node to li node
+  //tasks.innerHTML(item); // append li node to list
 });
+
+//document.addEventListener("DOMContentLoaded", () => {
+//  // your code here
+//  
+//  const newTaskForm = document.getElementById("create-task-form");
+//  
+// 
+//});
